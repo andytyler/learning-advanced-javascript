f5f2a8b96d07bcbd26dcf1b5f490e83e3b78cf0e
andytyler <atyler1@sheffield.ac.uk>
2019-08-10 10:54:10 +0100

    Created repo with js base file

---
+++ js-cheats.js
@@ -0,0 +1,23 @@
+// JavaScript Learnings & Cheet Sheet
+
+() => {
+	//dont have to use word function just use () and add the => to make it an arrow func
+	//annon functions if ony have one param you dont need to put the brackets arrpund the param
+	5 * 5; // dont need to have a return in an anon function if it is one one line
+	//it is implicit in anon functions
+};
+
+// Its a function can return a value
+// a method cant return a value
+// a method is implicitly passed the object on which it weas called
+
+// ARRAY METHODS
+
+// Filter
+// Map
+// Sort
+// Reduce
+// Every
+// Some
+// Find
+// Findindex
