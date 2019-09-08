eb994b961a3c557f062fbfa46855d3d33e8b087e
Andy Tyler <tylera07@mc-n358836.mynet>
2019-09-08 17:47:48 +0100

    Added map to the Array file

---
+++ Array.js
@@ -0,0 +1,8 @@
+
+// Array map function
+const startingArray = [1,2,3,4,5,6,7,8,9]
+
+const newArray = startingArray.map(item => {
+    return item + 4
+})
+console.log(startingArray,' Array.map() -> ',newArray)
